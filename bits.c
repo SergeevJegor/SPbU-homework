441cb8fa1c703704b0e3d5505542cb7f487ddf5c
Sergeev Jegor <egor.f.sergeev@gmail.com>
2017-10-08 16:47:24 +0300

    Added functions names

---
+++ bits.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int bitAnd(int x, int y);
+int bitXor(int x, int y);
+int thirdBits();
+int fitsBits(int x, int n);
+int sign(int x);
+int getByte(int x, int n);
+int logicalShift(int x, int n);
+int addOK(int x, int y);
+int bang(int x);
+int conditional(int x, int y, int z);
+int isPower2(int x);
+
+int main() {
+
+    system("pause");
+    return 0;
+}
