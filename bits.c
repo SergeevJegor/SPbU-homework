bd3fecb8ab8677c900e4ffc388c9e0a48d07cbfe
Sergeev Jegor <egor.f.sergeev@gmail.com>
2017-10-09 21:50:03 +0300

    Solved all except  addOK

--- bits.c
+++ bits.c
@@ -1,20 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int bitAnd(int x, int y);
-int bitXor(int x, int y);
-int thirdBits();
-int fitsBits(int x, int n);
-int sign(int x);
-int getByte(int x, int n);
-int logicalShift(int x, int n);
+int bitAnd(int x, int y);//
+int bitXor(int x, int y);//
+int thirdBits();//
+int fitsBits(int x, int n);//
+int signBits(int x);//
+int getByte(int x, int n);//
+int logicalShift(int x, int n);//
 int addOK(int x, int y);
-int bang(int x);
-int conditional(int x, int y, int z);
-int isPower2(int x);
+int bang(int x);//
+int conditional(int x, int y, int z);//
+int isPower2(int x);//
 
 int main() {
-
+    int x = 4, y = 7;
+    printf("%i", signBits(1024));
     system("pause");
     return 0;
 }
@@ -27,3 +28,44 @@ int bitXor(int x, int y){
     return ~(~(~x & y) & ~(x & ~y));
 }
 
+int thirdBits(){
+    int x = 73; /*001001001*/
+    x = (x << 18) | (x << 9) | (x);
+    x = (x << 6) | 9;
+    return x;
+}
+
+int fitsBits(int x, int n){
+    return !(((x & ~(x << 31)) + (~x & (x << 31))) >> (n + ~0));/* (x << 31) = 0, x>=0; =1, x<0 */
+}
+
+int signBits(int x){
+    return (!!x) | (x >> 31);
+}
+
+int getByte(int x, int n){
+    return (x >> (n << 3)) & 255;
+}
+
+int logicalShift(int x, int n){
+    return (x >> n) & ~((1 << 31) >> (n - 1));
+}
+
+int addOK(int x, int y){
+
+}
+
+int conditional(int x, int y, int z){
+    return ((((!!x) << 31) >> 31) & y) + ((((!x) << 31) >> 31) & z);
+}
+
+int bang(int x){
+    x = (x >> 16) | (x >> 8) | (x >> 4) | (x >> 2) | (x >> 1) | x;
+    return ~x & 1;
+}
+
+int isPower2(int x){
+    int sign = x >> 31;
+
+    return !((x & (x + (~0) ^ sign)) + !x);
+}
