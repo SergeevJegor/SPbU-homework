4520f2d4d9d84a605ee51e45264d4c1a144914ca
Sergeev Jegor <egor.f.sergeev@gmail.com>
2018-12-19 21:26:49 +0300

    Implemented 8th algorithm

---
+++ Semester_3/ParallelAlgorithms/src/main/java/com/company/algorithm8/Main.java
@@ -0,0 +1,45 @@
+package com.company.algorithm8;
+
+import com.company.parallel_scan.Tree;
+
+public class Main {
+
+    public static int threadsAmount;
+    public static int pairsAmount;
+    public static volatile int result;
+    public static volatile PairType[] prefixes;
+    public static PairType[] pairs;
+
+
+    public static void main(String[] args) {
+        setup(8, 4, null);
+        int interval = pairsAmount / threadsAmount;
+        Thread[] threads = new Thread[threadsAmount];
+        for (int i = 0; i < threadsAmount; i++) {
+            threads[i] = new Thread(new Calculator(i * interval, Math.min((i + 1) * interval, pairsAmount), i, pairs));
+            threads[i].start();
+        }
+        try {
+            for (int i = 0; i < threadsAmount; i++) {
+                threads[i].join();
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        Tree<PairType> tree = new Tree<PairType>(1, pairsAmount);
+        tree.treeSetup(pairs, 1, pairsAmount);
+        result = tree.upsweep().b;
+        System.out.println(result);
+    }
+
+    static void setup(int numOfThreads, int numOfPairs, PairType[] input) {
+        threadsAmount = numOfThreads;
+        pairsAmount = numOfPairs;
+        result = 0;
+        // pairs = input
+        pairs = new PairType[]{new PairType(1, 2), new PairType(0,3), new PairType(2,4), new PairType(1,5)};
+        prefixes = new PairType[threadsAmount];
+        for (int i = 0; i < threadsAmount; i++)
+            prefixes[i] = new PairType(0, 0);
+    }
+}
---
+++ Semester_3/ParallelAlgorithms/src/main/java/com/company/algorithm8/PairType.java
@@ -0,0 +1,21 @@
+package com.company.algorithm8;
+
+import com.company.parallel_scan.ValueType;
+
+public class PairType implements ValueType<PairType> {
+    int a;
+    int b;
+
+    public PairType(int a, int b) {
+        this.a = a;
+        this.b = b;
+    }
+
+    @Override
+    public PairType add(PairType p) {
+        PairType tmp = new PairType(0,0);
+        tmp.a = this.a * p.a;
+        tmp.b = p.a * this.b + p.b;
+        return tmp;
+    }
+}
