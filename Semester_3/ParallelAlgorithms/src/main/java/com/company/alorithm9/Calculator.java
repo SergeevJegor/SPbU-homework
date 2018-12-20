4a664427c548dcef39f6e79b5f7fe329217ea58a
Sergeev Jegor <egor.f.sergeev@gmail.com>
2018-12-20 06:33:18 +0300

    Implemented 9th algorithm

---
+++ Semester_3/ParallelAlgorithms/src/main/java/com/company/alorithm9/Calculator.java
@@ -0,0 +1,53 @@
+package com.company.alorithm9;
+
+import com.company.parallel_scan.Tree;
+
+public class Calculator implements Runnable {
+    private int from;
+    private int to;
+    private int threadID;
+    private IntBracketType[] brackets;
+    private IntBracketType[] prefixes;
+
+    public Calculator(int from, int to, int threadID, IntBracketType[] brackets, IntBracketType[] prefixes) {
+        this.from = from;
+        this.to = to;
+        this.threadID = threadID;
+        this.brackets = brackets;
+        this.prefixes = prefixes;
+    }
+
+    @Override
+    public void run() {
+        IntBracketType folder = new IntBracketType(0);
+        for (int i = from; i < to; i++)
+            folder = folder.add(brackets[i]);
+        prefixes[threadID] = folder;
+
+        synchronized (Main.synchronizer) {
+            Main.threadsFinished++;
+            if (Main.threadsFinished != Main.threadsAmount) {
+                try {
+                    Main.synchronizer.wait();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            } else {
+                Tree<IntBracketType> tree = new Tree<IntBracketType>(1, Main.threadsAmount);
+                tree.treeSetup(prefixes, 1, Main.threadsAmount);
+                folder = tree.upsweep();
+                tree.downsweep(Main.prefixes, folder);
+                Main.threadsFinished = 0;
+                Main.synchronizer.notifyAll();
+            }
+            IntBracketType total;
+            if (from != 0)
+                total = prefixes[threadID];
+            else
+                total = new IntBracketType(0);
+            brackets[from] = brackets[from].add(total);
+            for (int i = from + 1; i < to; i++)
+                brackets[i] = brackets[i-1].add(brackets[i]);
+        }
+    }
+}
---
+++ Semester_3/ParallelAlgorithms/src/main/java/com/company/alorithm9/Main.java
@@ -0,0 +1,56 @@
+package com.company.alorithm9;
+
+import static java.lang.System.exit;
+
+public class Main {
+    public static int threadsAmount = 4;
+    public static int stringSize = 8;
+    public static volatile IntBracketType[] prefixes;
+    public static IntBracketType[] parsedString;
+    public static final Object synchronizer = new Object();
+    public static int threadsFinished = 0;
+
+    public static void main(String[] agrs) {
+        setup(1, "((())))(");
+        int interval = stringSize / threadsAmount;
+        Thread[] threads = new Thread[threadsAmount];
+        for (int i = 0; i < threadsAmount; i++) {
+            threads[i] = new Thread(new Calculator(i * interval, Math.min((i + 1) * interval, stringSize), i, parsedString, prefixes));
+            threads[i].start();
+        }
+        try {
+            for (int i = 0; i < threadsAmount; i++) {
+                threads[i].join();
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        for (IntBracketType bracket : parsedString){
+            if (bracket.value == -1) {
+                System.out.println("Wrong bracket. Abort");
+                exit(1);
+            }
+        }
+        if (parsedString[stringSize - 1].value > 0) {
+            System.out.println("Wrong bracket. Abort");
+            exit(1);
+        }
+        System.out.println("OK");
+        exit(0);
+    }
+
+    public static void setup(int numOfThreads, String input) {
+        threadsAmount = numOfThreads;
+        stringSize = input.length();
+        prefixes = new IntBracketType[stringSize];
+        for (int i = 0; i < stringSize; i++)
+            prefixes[i] = new IntBracketType(0);
+        parsedString = new IntBracketType[stringSize];
+        // Parse string:
+        input = input.replace('(', '1').replace(')', '0');
+        String[] array = input.split("");
+        for (int i = 0; i < stringSize; i++)
+            parsedString[i] = new IntBracketType(Integer.parseInt(array[i]) == 0 ? -1 : 1);
+    }
+
+}
