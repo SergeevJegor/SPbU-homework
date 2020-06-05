8ef2b3d41d933eb233657594c52383dc9f0c39e9
Jegor Sergeev <egor.f.sergeev@gmail.com>
2020-06-05 13:49:13 +0300

    SNTP fix

--- Semester_6/telecommunication/main.py
+++ Semester_6/telecommunication/main.py
@@ -1,6 +1,7 @@
 import socket
 import struct
 import datetime
+import time
 
 
 class NTP:
@@ -13,12 +14,19 @@ class NTP:
 
     def get_time(self):
         data = '\x1b' + 47 * '\0'
+
+        t0 = time.time()
         self.client.sendto(data.encode('utf-8'), (self.address, self.port))
+        t3 = time.time()
 
         data, _ = self.client.recvfrom(1024)
         assert data, 'Error: got no data from NTP server'
+
         unpacked = struct.unpack('!12I', data)
-        ts = unpacked[10] + float(unpacked[11]) / 2 ** 32 - self.time_shift
+        t1 = unpacked[8] + float(unpacked[9]) / 2 ** 32 - self.time_shift
+        t2 = unpacked[10] + float(unpacked[11]) / 2 ** 32 - self.time_shift
+
+        ts = time.time() + (((t1 - t0) + (t2 - t3)) / 2)
         return ts
 
 
